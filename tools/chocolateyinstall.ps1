a851a2a5e7eecac4f906bf46210da97075f3592b
bthos <bthos@example.com>
2023-04-01 16:23:17 +0200

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.vrdesktop.net/files/VirtualDesktop.Streamer.Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url
+
+  softwareName  = 'Virtual Desktop Streamer*'
+
+  checksum      = 'E5EB0B067F6C6450038500584A0C1E2F506BB3D4FFE4B08877F3A608B4B753FB'
+  checksumType  = 'sha256'
+  checksum64    = 'E5EB0B067F6C6450038500584A0C1E2F506BB3D4FFE4B08877F3A608B4B753FB'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
